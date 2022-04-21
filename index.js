731c14cc9c1d3423c038c96a54deaeb8c38dd069
Kundan-Gemini <kundan.kumar@geminisolutions.com>
2022-04-21 11:44:45 +0530

    This is third change

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const fs = require("fs");
 // fs.writeFileSync('read.txt',"hello folks");
 
 /*When we again use this command it erases the previous data present in the file and insert the new data*/
-fs.writeFileSync(
-  "read.txt",
-  "hello folks cheking the working of same fs.writeFileSync"
-);
+
+// fs.writeFileSync(
+//   "read.txt",
+//   "hello folks cheking the working of same fs.writeFileSync"
+// );
+
+
+// Modifying the read.txt with new contents without deleting the existing contents
+fs.appendFileSync('read.txt'," Here we are apending this line to existing file")
+
