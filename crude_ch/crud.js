20e7955ef98d2842ecd8238f5a9e6d34018a4e72
Kundan-Gemini <kundan.kumar@geminisolutions.com>
2022-04-21 21:28:39 +0530

    Final commit in crud assignment

--- crude_ch/crud.js
+++ crude_ch/crud.js
@@ -1,4 +1,25 @@
 const fs=require("fs");
+const { cursorTo } = require("readline");
+
+// Creating a folder using node
+// fs.mkdirSync("CRUD_FOLDER");
 
 
-fs.mkdirSync("CRUD_FOLDER")
+// Make sure the above command is commented otherwise it will give error that file already exist
+// fs.writeFileSync('bio.txt',"My name is kundan");
+
+
+// Modifying the content of bio.txt without deleting existing content
+// fs.appendFileSync("bio.txt","I am Appending this line in file bio.txt")
+
+// Reading the file bio.txt
+// const reading_f=fs.readFileSync('bio.txt',"utf-8");
+// console.log(reading_f)
+
+
+// Renaming the file
+// fs.renameSync("bio.txt",'newBio.txt')
+
+//We can delete the file in node using fs.unlinkSync('File name')
+//Deleting the folder in node using fs.rmdirSync('filename')
+
