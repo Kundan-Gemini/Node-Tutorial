bc3b1d9eae7b777434667dba281833d33577d47b
Kundan-Gemini <kundan.kumar@geminisolutions.com>
2022-04-21 11:12:45 +0530

    This is First commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const fs= require('fs');
+
+
+// Creating a new file
+// fs.writeFileSync('read.txt',"hello folks");
+
+fs.writeFileSync('read.txt',"hello folks cheking the working of same fs.writeFileSync");
