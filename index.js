3d2be430f2d8a70dad97a96ad242f0741cf2c347
Kundan-Gemini <kundan.kumar@geminisolutions.com>
2022-04-21 11:33:07 +0530

    This is second change

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const fs= require('fs');
-
+const fs = require("fs");
 
 // Creating a new file
+
 // fs.writeFileSync('read.txt',"hello folks");
 
-fs.writeFileSync('read.txt',"hello folks cheking the working of same fs.writeFileSync");
+/*When we again use this command it erases the previous data present in the file and insert the new data*/
+fs.writeFileSync(
+  "read.txt",
+  "hello folks cheking the working of same fs.writeFileSync"
+);
