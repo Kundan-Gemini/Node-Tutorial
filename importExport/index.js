37f55eb07a6783a24f56a0223ff6d5e2cff74831
Kundan-Gemini <kundan.kumar@geminisolutions.com>
2022-04-22 18:26:37 +0530

    Importing exporting our own module

---
+++ importExport/index.js
@@ -0,0 +1,11 @@
+// Creating our own modules and using it => In node every individual file is known as module
+
+// Importing "open.js" suing require('')
+const add=require('./oper') /*Here "add" will get all the data exported from the "oper.js" file */
+
+// For addition
+// console.log(add(3,5))
+
+
+// For getting the name
+// console.log(add)
---
+++ importExport/oper.js
@@ -0,0 +1,12 @@
+// Creating the function
+
+// const add=(a,b)=>{
+//     return a+b;
+// }
+
+// By the below line we are making the file or function public so that it can be available for every other file
+// module.exports=add;
+
+//accessing a variable with string in other file
+// const name="kundan"
+// module.exports=name;
