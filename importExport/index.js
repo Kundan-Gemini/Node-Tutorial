679cd044aba36d4fd25468f9037f66a315f7680a
Kundan-Gemini <kundan.kumar@geminisolutions.com>
2022-04-23 13:40:20 +0530

    Importing exporting our own module

--- importExport/index.js
+++ importExport/index.js
@@ -1,7 +1,7 @@
 // Creating our own modules and using it => In node every individual file is known as module
 
 // Importing "open.js" suing require('')
-const add=require('./oper') /*Here "add" will get all the data exported from the "oper.js" file */
+// const add=require('./oper') /*Here "add" will get all the data exported from the "oper.js" file */
 
 // For addition
 // console.log(add(3,5))
@@ -9,3 +9,25 @@ const add=require('./oper') /*Here "add" will get all the data exported from the
 
 // For getting the name
 // console.log(add)
+
+// For subtraction
+// const sub=require('./oper')
+// console.log(sub(5,3))
+
+
+// For simultaneously calling the two functions
+// const oper=require('./oper')
+// console.log(oper) 
+/*This will show that oper will carry the data of two function in the form of object (- like this : -> { add: [Function: add], sub: [Function: sub] })and when we 
+need to access the content of the two function we need to use ".property_name" like we use in object */
+
+/*we can do the above work i.e using two function or varibales simultaneously using object destructuring */
+// const {add,sub}=require('./oper')
+// console.log(add(5,5));
+// console.log(sub(9,3));
+
+// Passing a variable 
+const {add,sub,name}=require('./oper')
+console.log(add(5,5));
+console.log(sub(9,3));
+console.log(name);
--- importExport/oper.js
+++ importExport/oper.js
@@ -4,9 +4,53 @@
 //     return a+b;
 // }
 
+
 // By the below line we are making the file or function public so that it can be available for every other file
 // module.exports=add;
 
 //accessing a variable with string in other file
 // const name="kundan"
 // module.exports=name;
+
+
+// For Subtraction
+// const sub=(a,b)=>{
+//     return a-b;
+// }
+
+// module.exports=sub;
+
+
+// => IMPORTANT !
+// For adding and subtracting simultaneously
+
+// const add=(a,b)=>{
+//     return a+b;
+// }
+
+// const sub=(a,b)=>{
+//         return a-b;
+//     }
+
+// module.exports.add=add;
+// module.exports.sub=sub;
+
+/*we can do the above work i.e using two function or varibales simultaneously using object destructuring */
+const add=(a,b)=>{
+    return a+b;
+}
+const sub=(a,b)=>{
+        return a-b;
+    }
+
+// module.exports.add=add;      1
+// module.exports.sub=sub;      2
+
+// we can modify above 1 and 2 using object destructuring
+
+// module.exports={add,sub}
+
+// Passing a name
+const name='Kundan';
+module.exports={add,sub,name}
+
